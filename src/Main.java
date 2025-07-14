47d8b39cc5ad2009b8c0f90d1cfd2625ccdce3c1
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 13:50:42 +0300

    Primitive and reference types

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,23 @@ public class Main {
         customerManager.Update();
 
         //classes are reference type
+
+        int number = 10;
+        int number1 = 20;
+        number1 =number;
+        number =30;
+        System.out.println(number1);
+
+
+        //primitive    STACK       = 	int, double, char, boolean, byte, short, long, float
+        //reference    STCAK/HEAP  =    String, Array, Object, Class, List, Map, Scanner vs.
+
+        int [] numbers1 = new int[]{1,2,3};
+        int [] numbers2 = new int[]{4,5,6};
+        numbers2 = numbers1;
+        numbers1[0] = 10;
+        System.out.println(numbers2[0]);
+
     }
 
 
