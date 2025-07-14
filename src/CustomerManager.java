5a10a20a5c11a518cf2e5bdf4b00d46b975a8dac
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 13:17:37 +0300

    Entrance to classes

---
+++ src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add () {
+        System.out.println("Costumer is added");
+    }
+
+    public void Remove () {
+        System.out.println("Costumer is removed");
+    }
+
+    public void Update () {
+        System.out.println("Costumer is updated");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int total = addition2(5,6,4,8,9,45,3,5);
-        System.out.println(total);
+
     }
 
 
-    public static int addition2 (int... numbers){
-        int total =0;
-        for (int number:numbers){
-            total +=number;
-        }
-        return total;
-    }
 }
+
