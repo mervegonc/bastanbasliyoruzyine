4ccd8d223f814db1656e304a3ed78f0ceac9b223
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 11:28:51 +0300

    Strings

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[][] cities = new String[3][3];
-        cities[0][0] = "Bursa";
-        cities[0][1] = "İstanbul";
-        cities[0][2] = "Bilecik";
-        cities[1][0] = "Çorum";
-        cities[1][1] = "Ankara";
-        cities[1][2] = "Konya";
-        cities[2][0] = "Kahramanmaraş";
-        cities[2][1] = "Gaziantep";
-        cities[2][2] = "Şanlıurfa";
-        for(int i=0; i<=2; i++){
-            for(int j =0; j<=2; j++){
-                System.out.println(cities[i][j]);
-            }
+       String message ="Today the weather is good.";
+        /* System.out.println("Number of elements of the text: "+message.length());
+        System.out.println("5. element: "+message.charAt(4));
+        System.out.println(message.concat(" hooray!"));
+        System.out.println(message.startsWith("A"));
+        System.out.println(message.endsWith("."));
+        char[] characters = new char[5];
+        message.getChars(0,5,characters,0);
+        System.out.println(characters);
+        System.out.println(message.indexOf('a'));
+
+        String newMessage =message.replace(' ','-');
+        System.out.println(newMessage);
+        System.out.println(message.substring(2,25));
+
+        for(String word:message.split(" ")){
+            System.out.println(word);
         }
+
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());*/
+
+
+
     }
 }
