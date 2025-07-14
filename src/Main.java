711191814090954615ce529daf79a0073a097027
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 12:35:39 +0300

    Call a number in the array

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        //BFF numbers 220-284
+        //we are looking for number in the array
+       int [] numbers = new int []{1,2,5,7,9,0};
+       int lookFor=78;
+       boolean exists=false;
 
-        int number = 220;
-        int number1 = 284;
-        int total = 0;
-        int total1 = 0;
-
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                total += i;
-            }
-
-        }
-
-        for (int i = 1; i < number1; i++) {
-            if (number1 % i == 0) {
-                total1 += i;
-            }
-        }
-
-
-        if (number == total1 && number1 == total) {
-            System.out.println("These two number are BFF's");
-        } else {
-            System.out.println("These two are strangers to each other ");
-        }
+       for(int number:numbers){
+           if(number ==lookFor){
+               exists = true;
+               break;
+           }
+       }
+       if(exists){
+           System.out.println("The nmber is exists");
+       }else {
+           System.out.println("The number doesn't exists");
+       }
     }
 }
