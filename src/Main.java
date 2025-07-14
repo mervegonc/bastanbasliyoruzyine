88ea68dd8e72042a4d7d1d916c5662f79e3c7945
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 13:06:01 +0300

    Variable arguments

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-        String message = "Today The weather is good";
-        String newMessage = cities();
-        System.out.println(newMessage);
-        int number =addition(5,7);
-        System.out.println(number);
+        int total = addition2(5,6,4,8,9,45,3,5);
+        System.out.println(total);
     }
 
 
-    public static void  add () {
-        System.out.println("Added");
-    }
-    public static void delete (){
-        System.out.println("Delete");
-    }
-    public static void update () {
-        System.out.println("Updated");
-    }
-
-    public static int addition (int number, int number1){
-
-        return  number + number1;
-
-    }
-
-    public static String cities (){
-        return "Ankara";
+    public static int addition2 (int... numbers){
+        int total =0;
+        for (int number:numbers){
+            total +=number;
+        }
+        return total;
     }
 }
