579c3dc032b4afe1bd38aa0247605493a08680d6
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 12:03:15 +0300

    The perfect number

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int number = 25; // Kontrol edilecek sayı
-       boolean isPrime = true; // Asal mı değil mi diye kontrol için bir bayrak (flag) değişkeni
+      int number = 6;
+      int total=0;
 
-        // Asal sayılar 1'den büyük olmalı
-        if (number <= 1) {
-            isPrime = false; // 1 ve daha küçük sayılar asal değildir
-        } else {
-            // 2'den başlayarak, sayının yarısına kadar olan tüm sayılarla bölen var mı diye bakılır
-            for (int i = 2; i <= number / 2; i++) {
-                if (number % i == 0) {
-                    // Eğer tam bölünüyorsa, asal değildir
-                    isPrime = false;
-                    break; // Daha fazla kontrol etmeye gerek yok, döngüden çık
-                }
-            }
-        }
+      for(int i=1;i<number;i++){
+          if(number % i==0){
+                total+=i;
+          }
 
-        // Sonuca göre ekrana yazdır
-        if (isPrime) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is NOT a prime number.");
+      }
+        if(total==number){
+            System.out.println("this is perfect number: "+number);
+        }else {
+            System.out.println("this is shity number:"+number);
         }
     }
 }
