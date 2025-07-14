511adf44b7d4118d209a4dd4c1cce1046ae67341
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 12:48:16 +0300

    Methods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        callANumber();
+    }
 
+    public static void callANumber() {
         //we are looking for number in the array
-       int [] numbers = new int []{1,2,5,7,9,0};
-       int lookFor=78;
-       boolean exists=false;
+        int[] numbers = new int[]{1, 2, 5, 7, 9, 0};
+        int lookFor = 7;
+        boolean exists = false;
 
-       for(int number:numbers){
-           if(number ==lookFor){
-               exists = true;
-               break;
-           }
-       }
-       if(exists){
-           System.out.println("The nmber is exists");
-       }else {
-           System.out.println("The number doesn't exists");
-       }
+        for (int number : numbers) {
+            if (number == lookFor) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists) {
+            giveMessage("The number is exists: "+lookFor);
+        } else {
+            giveMessage("The number isn't exists: "+lookFor);
+        }
+    }
+
+    public static void giveMessage(String message) {
+        System.out.println(message);
     }
 }
