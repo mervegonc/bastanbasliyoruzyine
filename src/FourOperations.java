fd51b256d345757eee6b1674cc9836f618f3a1ff
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 13:55:49 +0300

    Basiz operation

---
+++ src/FourOperations.java
@@ -0,0 +1,14 @@
+public class FourOperations {
+    public int addition (int number1, int number2) {
+        return number1 + number2;
+    }
+    public int substraction (int number1, int number2) {
+        return number1 - number2;
+    }
+    public int division (int number1, int number2) {
+        return number1 / number2;
+    }
+    public int multiplication (int number1, int number2) {
+        return number1 * number2;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        CustomerManager customerManager = new CustomerManager();
-        CustomerManager customerManager2 = new CustomerManager();
-        customerManager = customerManager2;
-        customerManager.Add();
-        customerManager.Remove();
-        customerManager.Update();
-
-        //classes are reference type
-
-        int number = 10;
-        int number1 = 20;
-        number1 =number;
-        number =30;
-        System.out.println(number1);
-
-
-        //primitive    STACK       = 	int, double, char, boolean, byte, short, long, float
-        //reference    STCAK/HEAP  =    String, Array, Object, Class, List, Map, Scanner vs.
-
-        int [] numbers1 = new int[]{1,2,3};
-        int [] numbers2 = new int[]{4,5,6};
-        numbers2 = numbers1;
-        numbers1[0] = 10;
-        System.out.println(numbers2[0]);
-
+      FourOperations fourOperations = new FourOperations();
+      int total =fourOperations.addition(5,9);
+        System.out.println(total);
     }
 
 
