094207d42c4dd5f69248555b2a892b4b34d660bb
merve-goncu <merve.gnc.946@gmail.com>
2025-07-14 10:24:18 +0300

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-       //for
-        for (int i=1; i<=10; i++){
-            System.out.println(i);
+      String student="Merve";
+      String student1="Engin";
+      String student2="Salih";
+
+
+      String[] students= new String[3];
+      students[0]="Merve";
+        students[1]="Engin";
+        students[2]="Salih";
+
+        for(int i=0; i< students.length; i++){
+            System.out.println(students[i]);
         }
-        System.out.println("For loop is over");
 
-        //while
-        int i=1;
-        while (i<=10){
-            System.out.println(i);
-            i++;
-
+        for(String studenty:students){
+            System.out.println(studenty);
         }
-        System.out.println("While loop is over");
-
-        //do while
-        int j=1;
-        do{
-            System.out.println(j);
-            j++;
-        }while (j<10);
-        System.out.println("Do-While loop is over");
-
-
-        int k=100;
-        do{
-            System.out.println(k);
-            k++;
-        }while (k<10);
-        System.out.println("Do-While loop is over");
     }
 }
