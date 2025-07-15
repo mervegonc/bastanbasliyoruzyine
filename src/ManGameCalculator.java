7bbd98b77c5c1fb742580eea3e91f0af0a645843
merve-goncu <merve.gnc.946@gmail.com>
2025-07-15 17:51:09 +0300

    Abstracts-Ovverride

--- src/ManGameCalculator.java
+++
@@ -1,6 +0,0 @@
-public class ManGameCalculator extends GameCalculator{
-
-    public void calculate(){
-        System.out.println("Your Point: 95");
-    }
-}
--- src/WomanGameCalculator.java
+++
@@ -1,5 +0,0 @@
-public class WomanGameCalculator extends GameCalculator{
-    public void calculate(){
-        System.out.println("Your Point: 95");
-    }
-}
